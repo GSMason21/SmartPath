6f1dd8c7ae51b652f7cc9a2a1732bb5a88fbbe19
Work <work@Masons-MacBook-Air.local>
2026-06-09 12:44:37 -0700

    Add Google Analytics GA4

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,28 @@
+import Script from 'next/script';
 import '../styles/globals.css';
 
+const GA_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {GA_ID && (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="ga-init" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_ID}', { page_path: window.location.pathname });
+            `}
+          </Script>
+        </>
+      )}
+      <Component {...pageProps} />
+    </>
+  );
 }
